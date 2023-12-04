cc14d1a5747032268ac1e7e17e1d4b7c3c48d969
Zehra Memmedzade <zehra@gmail.com>
2023-12-04 19:43:12 +0400

    Добавьте файлы проекта.

---
+++ DataSource/Bothes.cs
@@ -0,0 +1,79 @@
+﻿namespace DataSource;
+
+public interface DataSource
+{
+    public void writeData(string data) { }
+    public void readData() { }
+
+}
+
+public class FileDataSource : DataSource
+{
+    private string? filename;
+
+
+    public void writeData(string data) { }  
+    public void readData() { }
+
+    public  FileDataSource(string filename)
+    {
+       this.filename = filename;
+    }
+
+}
+
+public class DataSourceDecorator : DataSource
+{
+    private DataSource? wrappee;
+
+    public void writeData(string data)
+    {
+        // Your implementation here
+    }
+
+    public void readData()
+    {
+        
+    }
+
+    // Corrected constructor with the same name as the class
+    public  DataSourceDecorator(DataSource wrappee)
+    {
+        this.wrappee = wrappee;
+    }
+}
+
+
+public class EncryotionDecorator
+{ 
+
+
+    public void writeData(string data)
+    {
+
+    }
+
+    public void readData() { }
+
+
+  
+
+}
+
+public class CompressionDecorator
+{
+
+
+    public void writeData(string data)
+    {
+
+    }
+
+    public void readData() { }
+
+
+
+
+}
+
+
---
+++ DataSource/Program.cs
@@ -0,0 +1,11 @@
+﻿using DataSource;
+
+FileDataSource fileDataSource = new FileDataSource("File.json");
+
+
+DataSourceDecorator encryptedDataSource = new (fileDataSource);
+DataSourceDecorator compressedAndEncryptedDataSource = new (encryptedDataSource);
+
+compressedAndEncryptedDataSource.writeData("Salam Step");
+
+compressedAndEncryptedDataSource.readData();
